8aa5e085b5d14c2226ac918db3a42c6d944cee2b
decafandmac <eipeti.s2@gmail.com>
2022-12-26 20:39:44 +0900

    [KDP-3] SimpleConsumer 추가

--- src/main/kotlin/com/decafandmac/kafka/KafkaDemoProjectApplication.kt
+++ src/main/kotlin/com/decafandmac/kafka/KafkaDemoProjectApplication.kt
@@ -1,5 +1,6 @@
 package com.decafandmac.kafka
 
+import com.decafandmac.kafka.consumer.SimpleConsumer
 import com.decafandmac.kafka.producer.CustomProducerTemplate
 import com.decafandmac.kafka.producer.SimpleProducer
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -11,7 +12,10 @@ class KafkaDemoProjectApplication
 fun main(args: Array<String>) {
 	runApplication<KafkaDemoProjectApplication>(*args)
 
-	val simpleProducer = SimpleProducer("test", CustomProducerTemplate())
+//	val simpleProducer = SimpleProducer("test", CustomProducerTemplate())
+//
+//	simpleProducer.send("TestValue2")
 
-	simpleProducer.send("TestValue2")
+	val simpleConsumer = SimpleConsumer()
+	simpleConsumer("test")
 }
